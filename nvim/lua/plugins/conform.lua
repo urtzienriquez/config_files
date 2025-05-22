7306afc23d35e5629555b164a9c9a16a50a0ae50
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2025-05-22 15:47:48 +0200

    update

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,41 +1,32 @@
--- https://github.com/stevearc/conform.nvim
-
-local function configure_conform()
-    local conform = require("conform")
-    -- Keymaps
-    vim.keymap.set(
-        {"n", "v"},
-        "<leader>bf",
-        function()
-            conform.format(
-            {},
-            function(err)
-                if not err then
-                    local mode = vim.api.nvim_get_mode().mode
-                    if vim.startswith(string.lower(mode), "v") then
-                        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
-                    end
-                end
-            end
-            )
-        end,
-        {silent = true}
-    )
-    -- vim.o.formatexpr = "v:lua.require('conform').formatexpr()"
-    -- Configure conform
-    local opts = {
-        formatters_by_ft = {
-            julia = {"runic"},
-        },
-        default_format_opts = {
-            timeout_ms = 10000,
-        },
-    }
-    conform.setup(opts)
-end
-
 return {
-    "stevearc/conform.nvim",
-    commit = "db8a4a9edb217067b1d7a2e0362c74bfe9cc944d",
-    config = configure_conform,
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				lua = { "stylua" },
+				fortran = { "fprettify" },
+				python = { "black" },
+				julia = { "jupytext" },
+				r = { "air" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>bf", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
 }
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,15 +3,21 @@
 -- Set LSP keymappings in on_attach (i.e. only in buffers with LSP active)
 -- TODO: lspconfig recommend doing this in an LspAttach autocommand instead
 local on_attach = function(client, bufnr)
-    local opts = { buffer = bufnr, silent = true }
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "<leader>lrn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("n", "<leader>lrr", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "<leader>lca", function() vim.lsp.buf.code_action({apply = true}) end, opts)
-    vim.keymap.set("n", "<leader>lqf", function() vim.lsp.buf.code_action({apply = true}) end, opts)
-    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-    vim.keymap.set("v", "<leader>lfmt", function() vim.lsp.buf.format({timeout_ms = 1000000}) end, opts)
+	local opts = { buffer = bufnr, silent = true }
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+	vim.keymap.set("n", "<leader>lrn", vim.lsp.buf.rename, opts)
+	vim.keymap.set("n", "<leader>lrr", vim.lsp.buf.references, opts)
+	vim.keymap.set("n", "<leader>lca", function()
+		vim.lsp.buf.code_action({ apply = true })
+	end, opts)
+	vim.keymap.set("n", "<leader>lqf", function()
+		vim.lsp.buf.code_action({ apply = true })
+	end, opts)
+	vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+	vim.keymap.set("v", "<leader>lfmt", function()
+		vim.lsp.buf.format({ timeout_ms = 1000000 })
+	end, opts)
 end
 
 -- Setup lspconfig: capabilities is passed to lspconfig.$server.setup
@@ -19,52 +25,52 @@ end
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local default_opts = {
-    on_attach = on_attach,
-    capabilities = capabilities,
+	on_attach = on_attach,
+	capabilities = capabilities,
 }
 
 local servers = {
-    -- Go LSP (gopls)
-    gopls = default_opts,
-    -- R LSP (r-languageserver)
-    r_language_server = default_opts,
-    -- Julia LSP (LanguageServer.jl)
-    julials = vim.tbl_extend(
-        "force",
-        default_opts,
-        {
-            on_new_config = function(new_config, _)
-                local julia = vim.fn.expand("~/.julia/environments/nvim-lspconfig/bin/julia")
-                local REVISE_LANGUAGESERVER = false
-                if REVISE_LANGUAGESERVER then
-                    new_config.cmd[5] = (new_config.cmd[5]):gsub("using LanguageServer", "using Revise; using LanguageServer; LanguageServer.USE_REVISE[] = true")
-                elseif require("lspconfig").util.path.is_file(julia) then
-                    new_config.cmd[1] = julia
-                end
-            end,
-            -- This just adds dirname(fname) as a fallback (see nvim-lspconfig#1768).
-            root_dir = function(fname)
-                local util = require("lspconfig.util")
-                return util.root_pattern "Project.toml"(fname) or util.find_git_ancestor(fname) or
-                    util.path.dirname(fname)
-            end,
-            on_attach = function(client, bufnr)
-                on_attach(client, bufnr)
-                -- Disable automatic formatexpr since the LS.jl formatter isn't so nice.
-                vim.bo[bufnr].formatexpr = ""
-            end,
-        }
-    ),
+	-- Go LSP (gopls)
+	gopls = default_opts,
+	-- R LSP (r-languageserver)
+	r_language_server = default_opts,
+	-- python LSP
+	pyright = default_opts,
+	-- Julia LSP (LanguageServer.jl)
+	julials = vim.tbl_extend("force", default_opts, {
+		on_new_config = function(new_config, _)
+			local julia = vim.fn.expand("~/.julia/environments/nvim-lspconfig/bin/julia")
+			local REVISE_LANGUAGESERVER = false
+			if REVISE_LANGUAGESERVER then
+				new_config.cmd[5] = (new_config.cmd[5]):gsub(
+					"using LanguageServer",
+					"using Revise; using LanguageServer; LanguageServer.USE_REVISE[] = true"
+				)
+			elseif require("lspconfig").util.path.is_file(julia) then
+				new_config.cmd[1] = julia
+			end
+		end,
+		-- This just adds dirname(fname) as a fallback (see nvim-lspconfig#1768).
+		root_dir = function(fname)
+			local util = require("lspconfig.util")
+			return util.root_pattern("Project.toml")(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
+		end,
+		on_attach = function(client, bufnr)
+			on_attach(client, bufnr)
+			-- Disable automatic formatexpr since the LS.jl formatter isn't so nice.
+			vim.bo[bufnr].formatexpr = ""
+		end,
+	}),
 }
 
 local function configure_lsp()
-    lspconfig = require("lspconfig")
-    for name, opts in pairs(servers) do
-        lspconfig[name].setup(opts)
-    end
+	lspconfig = require("lspconfig")
+	for name, opts in pairs(servers) do
+		lspconfig[name].setup(opts)
+	end
 end
 
 return {
-    "neovim/nvim-lspconfig",
-    config = configure_lsp,
+	"neovim/nvim-lspconfig",
+	config = configure_lsp,
 }
