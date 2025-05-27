3580b1185c64d174a1c760dc79ddc5d3d7bd569f
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2025-05-27 09:13:36 +0200

    update

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -45,6 +45,7 @@ return {
 		{ "hrsh7th/cmp-nvim-lsp" },
 		{ "hrsh7th/cmp-buffer" },
 		{ "hrsh7th/cmp-path" },
+		{ "R-nvim/cmp-r" },
 		-- Snippet engine(s) required for nvim-cmp (expands things from LS)
 		{ "L3MON4D3/LuaSnip" },
 		{ "saadparwaiz1/cmp_luasnip" },
