474761994cf73cbb435c14171ac47b71e1bd39c4
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2025-06-02 08:23:17 +0200

    update

---
+++ surfingkeys.js
@@ -0,0 +1,313 @@
+// Compatibility Prefix
+const {
+    Clipboard,
+    Front,
+    Hints,
+    Normal,
+    RUNTIME,
+    Visual,
+    aceVimMap,
+    addSearchAlias,
+    cmap,
+    getClickableElements,
+    imap,
+    imapkey,
+    iunmap,
+    map,
+    mapkey,
+    readText,
+    removeSearchAlias,
+    tabOpenLink,
+    unmap,
+    unmapAllExcept,
+    vmapkey,
+    vunmap
+} = api;
+
+// ---- Settings ----
+Hints.setCharacters('asdfgyuiopqwertnmzxcvb');
+Hints.style('font-size: 12pt;')
+
+settings.defaultSearchEngine = 'd';
+settings.hintAlign = 'left';
+// settings.omnibarPosition = 'bottom';
+// settings.focusFirstCandidate = false;
+settings.focusAfterClosed = 'last';
+settings.scrollStepSize = 200;
+settings.tabsThreshold = 0;
+settings.modeAfterYank = 'Normal';
+
+// ---- Map -----
+
+// regional hints
+map('<Alt-l>', 'L')
+
+// save and Choose a bookmark
+map('am', 'ab');
+unmap('ab')
+map('m', 'b');
+
+// Choose a buffer/tab
+map('b', 'T');
+
+// History Back/Forward
+map('H', 'S');
+map('L', 'D');
+
+// --- Tabs ---
+// Tab Next/Prev
+map('J', 'R');
+map('K', 'E');
+
+
+addSearchAlias('b',  'brave', 'https://search.brave.com/search?q=', 's');
+
+
+// Theme
+
+settings.theme = `
+/* Edit these variables for easy theme making */
+:root {
+  /* Font */
+  --font: 'Source Code Pro', Ubuntu, sans;
+  --font-size: 12;
+  --font-weight: bold;
+
+  /* -------------------- */
+  /* --      NORD      -- */
+  /* -------------------- */
+  --fg: #E5E9F0;
+  --bg: #3B4252;
+  --bg-dark: #2E3440;
+  --border: #4C566A;
+  --main-fg: #88C0D0;
+  --accent-fg: #A3BE8C;
+  --info-fg: #5E81AC;
+  --select: #4C566A;
+
+  /* Unused Alternate Colors */
+  /* --orange: #D08770; */
+  /* --red: #BF616A; */
+  /* --yellow: #EBCB8B; */
+
+}
+
+/* ---------- Generic ---------- */
+.sk_theme {
+background: var(--bg);
+color: var(--fg);
+  background-color: var(--bg);
+  border-color: var(--border);
+  font-family: var(--font);
+  font-size: var(--font-size);
+  font-weight: var(--font-weight);
+}
+
+input {
+  font-family: var(--font);
+  font-weight: var(--font-weight);
+}
+
+.sk_theme tbody {
+  color: var(--fg);
+}
+
+.sk_theme input {
+  color: var(--fg);
+}
+
+/* Hints */
+#sk_hints .begin {
+  color: var(--accent-fg) !important;
+}
+
+#sk_tabs .sk_tab {
+  background: var(--bg-dark);
+  border: 1px solid var(--border);
+}
+
+#sk_tabs .sk_tab_title {
+  color: var(--fg);
+}
+
+#sk_tabs .sk_tab_url {
+  color: var(--main-fg);
+}
+
+#sk_tabs .sk_tab_hint {
+  background: var(--bg);
+  border: 1px solid var(--border);
+  color: var(--accent-fg);
+}
+
+.sk_theme #sk_frame {
+  background: var(--bg);
+  opacity: 0.2;
+  color: var(--accent-fg);
+}
+
+/* ---------- Omnibar ---------- */
+/* Uncomment this and use settings.omnibarPosition = 'bottom' for Pentadactyl/Tridactyl style bottom bar */
+/* .sk_theme#sk_omnibar {
+  width: 100%;
+  left: 0;
+} */
+
+.sk_theme .title {
+  color: var(--accent-fg);
+}
+
+.sk_theme .url {
+  color: var(--main-fg);
+}
+
+.sk_theme .annotation {
+  color: var(--accent-fg);
+}
+
+.sk_theme .omnibar_highlight {
+  color: var(--accent-fg);
+}
+
+.sk_theme .omnibar_timestamp {
+  color: var(--info-fg);
+}
+
+.sk_theme .omnibar_visitcount {
+  color: var(--accent-fg);
+}
+
+.sk_theme #sk_omnibarSearchResult ul li:nth-child(odd) {
+  background: var(--bg-dark);
+}
+
+.sk_theme #sk_omnibarSearchResult ul li.focused {
+  background: var(--border);
+}
+
+.sk_theme #sk_omnibarSearchArea {
+  border-top-color: var(--border);
+  border-bottom-color: var(--border);
+}
+
+.sk_theme #sk_omnibarSearchArea input,
+.sk_theme #sk_omnibarSearchArea span {
+  font-size: var(--font-size);
+}
+
+.sk_theme .separator {
+  color: var(--accent-fg);
+}
+
+/* ---------- Popup Notification Banner ---------- */
+#sk_banner {
+  font-family: var(--font);
+  font-size: var(--font-size);
+  font-weight: var(--font-weight);
+  background: var(--bg);
+  border-color: var(--border);
+  color: var(--fg);
+  opacity: 0.9;
+}
+
+/* ---------- Popup Keys ---------- */
+#sk_keystroke {
+  background-color: var(--bg);
+}
+
+.sk_theme kbd .candidates {
+  color: var(--info-fg);
+}
+
+.sk_theme span.annotation {
+  color: var(--accent-fg);
+}
+
+/* ---------- Popup Translation Bubble ---------- */
+#sk_bubble {
+  background-color: var(--bg) !important;
+  color: var(--fg) !important;
+  border-color: var(--border) !important;
+}
+
+#sk_bubble * {
+  color: var(--fg) !important;
+}
+
+#sk_bubble div.sk_arrow div:nth-of-type(1) {
+  border-top-color: var(--border) !important;
+  border-bottom-color: var(--border) !important;
+}
+
+#sk_bubble div.sk_arrow div:nth-of-type(2) {
+  border-top-color: var(--bg) !important;
+  border-bottom-color: var(--bg) !important;
+}
+
+/* ---------- Search ---------- */
+#sk_status,
+#sk_find {
+  font-size: var(--font-size);
+  border-color: var(--border);
+}
+
+.sk_theme kbd {
+  background: var(--bg-dark);
+  border-color: var(--border);
+  box-shadow: none;
+  color: var(--fg);
+}
+
+.sk_theme .feature_name span {
+  color: var(--main-fg);
+}
+
+/* ---------- ACE Editor ---------- */
+#sk_editor {
+  background: var(--bg-dark) !important;
+  height: 50% !important;
+  /* Remove this to restore the default editor size */
+}
+
+.ace_dialog-bottom {
+  border-top: 1px solid var(--bg) !important;
+}
+
+.ace-chrome .ace_print-margin,
+.ace_gutter,
+.ace_gutter-cell,
+.ace_dialog {
+  background: var(--bg) !important;
+}
+
+.ace-chrome {
+  color: var(--fg) !important;
+}
+
+.ace_gutter,
+.ace_dialog {
+  color: var(--fg) !important;
+}
+
+.ace_cursor {
+  color: var(--fg) !important;
+}
+
+.normal-mode .ace_cursor {
+  background-color: var(--fg) !important;
+  border: var(--fg) !important;
+  opacity: 0.7 !important;
+}
+
+.ace_marker-layer .ace_selection {
+  background: var(--select) !important;
+}
+
+.ace_editor,
+.ace_dialog span,
+.ace_dialog input {
+  font-family: var(--font);
+  font-size: var(--font-size);
+  font-weight: var(--font-weight);
+}
+`;
