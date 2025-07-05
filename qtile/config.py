58f50568bb68695d3d88d9045cf4da68a9189b88
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2025-07-05 02:27:05 +0200

    update

--- qtile/config.py
+++ qtile/config.py
@@ -42,7 +42,7 @@ keys = [
     ),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    Key([mod, "control"], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
