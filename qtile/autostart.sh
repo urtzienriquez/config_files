44b556d3fa09b5329eb8b6fcc5647873e620228b
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2025-07-15 22:29:25 +0200

    update

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 ~/.config/polybar/launch.sh &
+xset dpms 0 0 0
+xset -dpms
+xset s off
 unclutter --iddle 1 &
 xautolock -time 5 -locker "/home/urtzi/.config/qtile/smartlock.sh" &
 autorandr --change
