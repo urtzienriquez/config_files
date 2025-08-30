3644b3a0f309b04448f5e201cdd9b78a9138145c
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2025-08-30 17:23:09 +0200

    update

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -45,6 +45,173 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- ========================================
+-- PLUGIN-DEPENDENT KEYMAPS
+-- ========================================
+
+-- Create autocommands that will set up keymaps after plugins load
+vim.api.nvim_create_autocmd("User", {
+	pattern = "VeryLazy",
+	callback = function()
+		-- ========================================
+		-- UFO (code folding) keymaps
+		-- ========================================
+		local ufo_ok, ufo = pcall(require, "ufo")
+		if ufo_ok then
+			vim.keymap.set("n", "zR", ufo.openAllFolds, { desc = "Open all folds" })
+			vim.keymap.set("n", "zM", ufo.closeAllFolds, { desc = "Close all folds" })
+		end
+
+		-- ========================================
+		-- Oil file explorer keymaps
+		-- ========================================
+		local oil_ok, oil = pcall(require, "oil")
+		if oil_ok then
+			vim.keymap.set("n", "<leader>-", function()
+				if vim.bo.filetype == "oil" then
+					oil.close()
+				else
+					oil.open()
+				end
+			end, { desc = "Toggle Oil file explorer" })
+		end
+
+		-- ========================================
+		-- Snacks picker (fuzzy finding) keymaps
+		-- ========================================
+		if _G.Snacks and _G.Snacks.picker then
+			vim.keymap.set("n", "<leader>fs", function()
+				Snacks.picker.smart()
+			end, { desc = "Smart files" })
+			vim.keymap.set("n", "<leader>ff", function()
+				Snacks.picker.files()
+			end, { desc = "Find files" })
+			vim.keymap.set("n", "<leader>fb", function()
+				Snacks.picker.buffers({
+					win = { input = { keys = { ["<C-d>"] = { "bufdelete", mode = { "i", "n" } } } } },
+				})
+			end, { desc = "Find buffers" })
+			vim.keymap.set("n", "<leader>fg", function()
+				Snacks.picker.grep()
+			end, { desc = "Find grep" })
+			vim.keymap.set("n", "<leader>fG", function()
+				Snacks.picker.grep_buffers()
+			end, { desc = "Grep in buffers" })
+			vim.keymap.set("n", "<leader>fd", function()
+				Snacks.picker.diagnostics_buffer()
+			end, { desc = "Find diagnostics" })
+			vim.keymap.set("n", "<leader>fk", function()
+				Snacks.picker.keymaps()
+			end, { desc = "Find keymaps" })
+		end
+
+		-- ========================================
+		-- Conform (formatting) keymaps
+		-- ========================================
+		local conform_ok, conform = pcall(require, "conform")
+		if conform_ok then
+			vim.keymap.set({ "n", "v" }, "<leader>bf", function()
+				conform.format({
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 500,
+				})
+			end, { desc = "Format file or range" })
+		end
+
+		-- ========================================
+		-- Tmux Navigator keymaps
+		-- ========================================
+		vim.keymap.set("n", "<c-h>", "<cmd>TmuxNavigateLeft<cr>", { desc = "Navigate left (tmux)" })
+		vim.keymap.set("n", "<c-j>", "<cmd>TmuxNavigateDown<cr>", { desc = "Navigate down (tmux)" })
+		vim.keymap.set("n", "<c-k>", "<cmd>TmuxNavigateUp<cr>", { desc = "Navigate up (tmux)" })
+		vim.keymap.set("n", "<c-l>", "<cmd>TmuxNavigateRight<cr>", { desc = "Navigate right (tmux)" })
+		vim.keymap.set("n", "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>", { desc = "Navigate previous (tmux)" })
+
+		-- ========================================
+		-- Snacks (misc) keymaps
+		-- ========================================
+		if _G.Snacks then
+			-- Zen mode
+			vim.keymap.set("n", "<leader>z", function()
+				Snacks.zen()
+			end, { desc = "Toggle Zen Mode" })
+			vim.keymap.set("n", "<leader>Z", function()
+				Snacks.zen.zoom()
+			end, { desc = "Toggle Zoom" })
+
+			-- Scratch buffer
+			vim.keymap.set("n", "<leader>.", function()
+				Snacks.scratch()
+			end, { desc = "Toggle Scratch Buffer" })
+			vim.keymap.set("n", "<leader>S", function()
+				Snacks.scratch.select()
+			end, { desc = "Select Scratch Buffer" })
+
+			-- Notifications
+			vim.keymap.set("n", "<leader>n", function()
+				Snacks.notifier.show_history()
+			end, { desc = "Notification History" })
+			vim.keymap.set("n", "<leader>un", function()
+				Snacks.notifier.hide()
+			end, { desc = "Dismiss All Notifications" })
+
+			-- Buffer management
+			vim.keymap.set("n", "<leader>bd", function()
+				Snacks.bufdelete()
+			end, { desc = "Delete Buffer" })
+
+			-- File operations
+			vim.keymap.set("n", "<leader>R", function()
+				Snacks.rename.rename_file()
+			end, { desc = "Rename File" })
+
+			-- Git
+			vim.keymap.set({ "n", "v" }, "<leader>gB", function()
+				Snacks.gitbrowse()
+			end, { desc = "Git Browse" })
+			vim.keymap.set("n", "<leader>ll", function()
+				Snacks.lazygit.log()
+			end, { desc = "Lazygit log" })
+			vim.keymap.set("n", "<leader>lg", function()
+				Snacks.lazygit()
+			end, { desc = "Lazygit" })
+
+			-- Terminal
+			vim.keymap.set("n", "<leader>t", function()
+				Snacks.terminal()
+			end, { desc = "Toggle Terminal" })
+			vim.keymap.set("n", "<c-_>", function()
+				Snacks.terminal()
+			end, { desc = "Toggle Terminal" })
+
+			-- Word jumping
+			vim.keymap.set({ "n", "t" }, "]]", function()
+				Snacks.words.jump(vim.v.count1)
+			end, { desc = "Next Reference" })
+			vim.keymap.set({ "n", "t" }, "[[", function()
+				Snacks.words.jump(-vim.v.count1)
+			end, { desc = "Prev Reference" })
+
+			-- Neovim news
+			vim.keymap.set("n", "<leader>N", function()
+				Snacks.win({
+					file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+					width = 0.6,
+					height = 0.6,
+					wo = {
+						spell = false,
+						wrap = false,
+						signcolumn = "yes",
+						statuscolumn = " ",
+						conceallevel = 3,
+					},
+				})
+			end, { desc = "Neovim News" })
+		end
+	end,
+})
+
+-- ========================================
 -- LSP KEYMAPS (set when LSP attaches)
 -- ========================================
 vim.api.nvim_create_autocmd("LspAttach", {
@@ -57,336 +224,54 @@ vim.api.nvim_create_autocmd("LspAttach", {
 })
 
 -- ========================================
--- PLUGIN KEYMAPS SETUP
+-- WHICH-KEY GROUPS AND ORGANIZATION
 -- ========================================
-
--- Simple function to set up keymaps with multiple attempts
-local function setup_plugin_keymaps()
-	local max_attempts = 30
-	local attempt = 0
-
-	local function try_setup()
-		attempt = attempt + 1
-
-		-- UFO keymaps
-		local ufo_ok, ufo = pcall(require, "ufo")
-		if
-			ufo_ok
-			and not vim.tbl_isempty(vim.tbl_filter(function(k)
-					return k.lhs == "zR"
-				end, vim.api.nvim_get_keymap("n")))
-				== false
-		then
-			vim.keymap.set("n", "zR", ufo.openAllFolds, { desc = "Open all folds" })
-			vim.keymap.set("n", "zM", ufo.closeAllFolds, { desc = "Close all folds" })
-		end
-
-		-- Oil keymaps
-		local oil_ok, oil = pcall(require, "oil")
-		if
-			oil_ok
-			and not vim.tbl_isempty(vim.tbl_filter(function(k)
-					return k.lhs == "<Leader>-"
-				end, vim.api.nvim_get_keymap("n")))
-				== false
-		then
-			vim.keymap.set("n", "<leader>-", function()
-				if vim.bo.filetype == "oil" then
-					oil.close()
-				else
-					oil.open()
-				end
-			end, { desc = "Toggle Oil file explorer" })
-		end
-
-		-- Snacks keymaps
-		if _G.Snacks then
-			if
-				_G.Snacks.picker
-				and not vim.tbl_isempty(vim.tbl_filter(function(k)
-						return k.lhs == "<Leader>ff"
-					end, vim.api.nvim_get_keymap("n")))
-					== false
-			then
-				vim.keymap.set("n", "<leader>fs", function()
-					Snacks.picker.smart()
-				end, { desc = "Smart files" })
-				vim.keymap.set("n", "<leader>ff", function()
-					Snacks.picker.files()
-				end, { desc = "Find files" })
-				vim.keymap.set("n", "<leader>fb", function()
-					Snacks.picker.buffers({
-						win = { input = { keys = { ["<C-d>"] = { "bufdelete", mode = { "i", "n" } } } } },
-					})
-				end, { desc = "Find buffers" })
-				vim.keymap.set("n", "<leader>fg", function()
-					Snacks.picker.grep()
-				end, { desc = "Find grep" })
-				vim.keymap.set("n", "<leader>fG", function()
-					Snacks.picker.grep_buffers()
-				end, { desc = "Grep in buffers" })
-				vim.keymap.set("n", "<leader>fd", function()
-					Snacks.picker.diagnostics_buffer()
-				end, { desc = "Find diagnostics" })
-				vim.keymap.set("n", "<leader>fk", function()
-					Snacks.picker.keymaps()
-				end, { desc = "Find keymaps" })
-			end
-
-			-- Other Snacks functions
-			if
-				not vim.tbl_isempty(vim.tbl_filter(function(k)
-					return k.lhs == "<Leader>z"
-				end, vim.api.nvim_get_keymap("n"))) == false
-			then
-				vim.keymap.set("n", "<leader>z", function()
-					Snacks.zen()
-				end, { desc = "Toggle Zen Mode" })
-				vim.keymap.set("n", "<leader>Z", function()
-					Snacks.zen.zoom()
-				end, { desc = "Toggle Zoom" })
-				vim.keymap.set("n", "<leader>.", function()
-					Snacks.scratch()
-				end, { desc = "Toggle Scratch Buffer" })
-				vim.keymap.set("n", "<leader>S", function()
-					Snacks.scratch.select()
-				end, { desc = "Select Scratch Buffer" })
-				vim.keymap.set("n", "<leader>n", function()
-					Snacks.notifier.show_history()
-				end, { desc = "Notification History" })
-				vim.keymap.set("n", "<leader>un", function()
-					Snacks.notifier.hide()
-				end, { desc = "Dismiss All Notifications" })
-				vim.keymap.set("n", "<leader>bd", function()
-					Snacks.bufdelete()
-				end, { desc = "Delete Buffer" })
-				vim.keymap.set("n", "<leader>cR", function()
-					Snacks.rename.rename_file()
-				end, { desc = "Rename File" })
-				vim.keymap.set({ "n", "v" }, "<leader>gB", function()
-					Snacks.gitbrowse()
-				end, { desc = "Git Browse" })
-				vim.keymap.set("n", "<leader>ll", function()
-					Snacks.lazygit.log()
-				end, { desc = "Lazygit log" })
-				vim.keymap.set("n", "<leader>lg", function()
-					Snacks.lazygit()
-				end, { desc = "Lazygit" })
-				vim.keymap.set("n", "<leader>t", function()
-					Snacks.terminal()
-				end, { desc = "Toggle Terminal" })
-				vim.keymap.set("n", "<c-_>", function()
-					Snacks.terminal()
-				end, { desc = "Toggle Terminal" })
-				vim.keymap.set({ "n", "t" }, "]]", function()
-					Snacks.words.jump(vim.v.count1)
-				end, { desc = "Next Reference" })
-				vim.keymap.set({ "n", "t" }, "[[", function()
-					Snacks.words.jump(-vim.v.count1)
-				end, { desc = "Prev Reference" })
-				vim.keymap.set("n", "<leader>N", function()
-					Snacks.win({
-						file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-						width = 0.6,
-						height = 0.6,
-						wo = { spell = false, wrap = false, signcolumn = "yes", statuscolumn = " ", conceallevel = 3 },
-					})
-				end, { desc = "Neovim News" })
-			end
-		end
-
-		-- Conform keymaps
-		local conform_ok, conform = pcall(require, "conform")
-		if
-			conform_ok
-			and not vim.tbl_isempty(vim.tbl_filter(function(k)
-					return k.lhs == "<Leader>bf"
-				end, vim.api.nvim_get_keymap("n")))
-				== false
-		then
-			vim.keymap.set({ "n", "v" }, "<leader>bf", function()
-				conform.format({ lsp_fallback = true, async = false, timeout_ms = 500 })
-			end, { desc = "Format file or range" })
-		end
-
-		-- Tmux Navigator keymaps
-		if
-			vim.fn.exists(":TmuxNavigateLeft") == 2
-			and not vim.tbl_isempty(vim.tbl_filter(function(k)
-					return k.lhs == "<C-H>"
-				end, vim.api.nvim_get_keymap("n")))
-				== false
-		then
-			vim.keymap.set("n", "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>", { desc = "Navigate left (tmux)" })
-			vim.keymap.set("n", "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>", { desc = "Navigate down (tmux)" })
-			vim.keymap.set("n", "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>", { desc = "Navigate up (tmux)" })
-			vim.keymap.set("n", "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>", { desc = "Navigate right (tmux)" })
-			vim.keymap.set("n", "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>", { desc = "Navigate previous (tmux)" })
-		end
-
-		-- Slime keymaps (global)
-		if
-			vim.fn.exists("*slime#send") == 1
-			and _G.open_r_repl
-			and not vim.tbl_isempty(vim.tbl_filter(function(k)
-					return k.lhs == "<Leader>or"
-				end, vim.api.nvim_get_keymap("n")))
-				== false
-		then
-			vim.keymap.set("n", "<leader>or", _G.open_r_repl, { desc = "Open R (radian) REPL" })
-			vim.keymap.set("n", "<leader>op", _G.open_python_repl, { desc = "Open Python REPL" })
-			vim.keymap.set("n", "<leader>oj", _G.open_julia_repl, { desc = "Open Julia REPL" })
-			vim.keymap.set("n", "<leader>om", _G.open_matlab_repl, { desc = "Open MATLAB REPL" })
-			vim.keymap.set("n", "<leader>cr", _G.close_repl, { desc = "Close REPL" })
-			vim.keymap.set("n", "<leader>sd", _G.sync_working_directory, { desc = "Sync REPL directory" })
-
-			if vim.fn.exists(":SlimeConfig") == 2 then
-				vim.keymap.set("n", "<leader>sc", "<Cmd>SlimeConfig<CR>", { desc = "Configure slime" })
-			end
-
-			vim.keymap.set("n", "<leader>rr", function()
-				local filename = vim.fn.expand("%:t")
-				if filename:match("%.rmd$") or filename:match("%.Rmd$") then
-					if _G.render_rmarkdown then
-						_G.render_rmarkdown()
-					end
-				elseif filename:match("%.jmd$") then
-					if _G.render_jmarkdown then
-						_G.render_jmarkdown()
-					end
-				elseif filename:match("%.qmd$") or filename:match("%.Qmd$") then
-					if _G.render_quarto then
-						_G.render_quarto()
-					end
-				else
-					vim.notify("Not a renderable markdown file (.rmd, .jmd, or .qmd)", vim.log.levels.WARN)
-				end
-			end, { desc = "Render markdown document" })
-		end
-
-		-- Try again if not all plugins are ready
-		if attempt < max_attempts then
-			vim.defer_fn(try_setup, 500)
-		else
-			-- Set up which-key groups
-			vim.defer_fn(function()
-				local wk_ok, wk = pcall(require, "which-key")
-				if wk_ok then
-					wk.add({
-						{ "<leader>f", group = "Find" },
-						{ "<leader>b", group = "Buffer" },
-						{ "<leader>c", group = "Code" },
-						{ "<leader>g", group = "Git" },
-						{ "<leader>l", group = "Lazy/Logs" },
-						{ "<leader>o", group = "Open REPL" },
-						{ "<leader>r", group = "Render/Run" },
-						{ "<leader>s", group = "Send/Slime" },
-						{ "<leader>u", group = "UI/Toggle" },
-					})
-				end
-			end, 1000)
-		end
-	end
-
-	-- Start trying to set up keymaps
-	vim.defer_fn(try_setup, 1000)
-end
-
--- Start setup after VimEnter
-vim.api.nvim_create_autocmd("VimEnter", {
+vim.api.nvim_create_autocmd("User", {
+	pattern = "VeryLazy",
 	callback = function()
-		setup_plugin_keymaps()
-	end,
-})
+		local wk_ok, wk = pcall(require, "which-key")
+		if wk_ok then
+			wk.add({
+				-- Main groups
+				{ "<leader>f", group = "[f]ind" },
+				{ "<leader>b", group = "[b]uffer" },
+				{ "<leader>c", group = "[c]lose REPL" },
+				{ "<leader>g", group = "[g]it" },
+				{ "<leader>l", group = "[l]azygit" },
+				{ "<leader>o", group = "[o]pen REPL" },
+				{ "<leader>r", group = "[r]ender" },
+				{ "<leader>s", group = "[s]end / slime" },
+				{ "<leader>u", group = "[u]i / toggle" },
 
--- ========================================
--- FILETYPE-SPECIFIC SLIME KEYMAPS
--- ========================================
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "python", "julia", "r", "matlab", "markdown", "rmd", "quarto" },
-	callback = function()
-		local opts = { buffer = true, silent = true }
-		local current_ft = vim.bo.filetype
+				-- Specific mappings for better organization
+				{ "<leader>or", desc = "Open R REPL" },
+				{ "<leader>op", desc = "Open Python REPL" },
+				{ "<leader>oj", desc = "Open Julia REPL" },
+				{ "<leader>om", desc = "Open MATLAB REPL" },
+				{ "<leader>cr", desc = "Close REPL" },
 
-		-- Set cell delimiter
-		if current_ft == "matlab" then
-			vim.b.slime_cell_delimiter = "%% "
-		else
-			vim.b.slime_cell_delimiter = "# %%"
+				{ "<leader>ff", desc = "Find files" },
+				{ "<leader>fb", desc = "Find buffers" },
+				{ "<leader>fg", desc = "Find with grep" },
+				{ "<leader>fG", desc = "Grep in buffers" },
+				{ "<leader>fd", desc = "Find diagnostics" },
+				{ "<leader>fk", desc = "Find keymaps" },
+				{ "<leader>fs", desc = "Smart find" },
+
+				{ "<leader>bf", desc = "Format buffer" },
+				{ "<leader>bd", desc = "Delete buffer" },
+
+				{ "<leader>sc", desc = "Configure slime" },
+				{ "<leader>sd", desc = "Sync directory" },
+				{ "<leader>sx", desc = "Send cell" },
+				{ "<leader>sb", desc = "Send buffer" },
+
+				{ "<leader>rr", desc = "Render document" },
+
+				{ "<leader>lg", desc = "Lazygit" },
+				{ "<leader>ll", desc = "Lazygit log" },
+				{ "<leader>gB", desc = "Git browse" },
+			})
 		end
-
-		-- Set up buffer-local slime keymaps with simple retry
-		local function setup_buffer_keymaps()
-			local attempts = 0
-			local function try_buffer_setup()
-				attempts = attempts + 1
-
-				if vim.fn.exists("*slime#send") == 1 and _G.smart_send and _G.send_cell then
-					-- Main smart send - Enter key
-					vim.keymap.set("n", "<Enter>", function()
-						_G.smart_send("n")
-					end, vim.tbl_extend("force", opts, { desc = "Smart send" }))
-
-					vim.keymap.set("v", "<Enter>", function()
-						_G.smart_send("v")
-					end, vim.tbl_extend("force", opts, { desc = "Send selection" }))
-
-					-- Send cell
-					vim.keymap.set(
-						"n",
-						"<leader>sx",
-						_G.send_cell,
-						vim.tbl_extend("force", opts, { desc = "Send cell" })
-					)
-
-					-- Send buffer
-					if vim.fn.exists(":SlimeSend") == 2 then
-						vim.keymap.set(
-							"n",
-							"<leader>sb",
-							"<Cmd>%SlimeSend<CR>",
-							vim.tbl_extend("force", opts, { desc = "Send buffer" })
-						)
-					end
-
-					-- File-specific render keybinds
-					local filename = vim.fn.expand("%:t")
-					if filename:match("%.rmd$") or filename:match("%.Rmd$") then
-						if _G.render_rmarkdown then
-							vim.keymap.set(
-								"n",
-								"<leader>rr",
-								_G.render_rmarkdown,
-								vim.tbl_extend("force", opts, { desc = "Render R Markdown" })
-							)
-						end
-					elseif filename:match("%.jmd$") then
-						if _G.render_jmarkdown then
-							vim.keymap.set(
-								"n",
-								"<leader>rr",
-								_G.render_jmarkdown,
-								vim.tbl_extend("force", opts, { desc = "Render Julia Markdown" })
-							)
-						end
-					elseif filename:match("%.qmd$") or filename:match("%.Qmd$") then
-						if _G.render_quarto then
-							vim.keymap.set(
-								"n",
-								"<leader>rr",
-								_G.render_quarto,
-								vim.tbl_extend("force", opts, { desc = "Render Quarto" })
-							)
-						end
-					end
-				elseif attempts < 20 then
-					vim.defer_fn(try_buffer_setup, 500)
-				end
-			end
-
-			vim.defer_fn(try_buffer_setup, 500)
-		end
-
-		setup_buffer_keymaps()
 	end,
 })
