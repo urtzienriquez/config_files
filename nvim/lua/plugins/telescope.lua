14551e1716dc3f1e7dbfe863dd685c807db2c3f8
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2025-09-03 01:07:49 +0200

    update

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,48 +1,61 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("telescope").setup({
-				defaults = {
-					layout_strategy = "horizontal",
-					layout_config = {
-						prompt_position = "top",
-					},
-					sorting_strategy = "ascending",
-				},
-				pickers = {
-					buffers = {
-						show_all_buffers = true,
-						sort_lastused = true,
-						theme = "dropdown",
-						previewer = false,
-						mappings = {
-							i = {
-								["<c-d>"] = "delete_buffer",
-							},
-						},
-					},
-					find_files = {
-						hidden = true,
-						no_ignore = true,
-						file_ignore_patterns = { ".git/" },
-					},
-				},
-			})
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      -- Correctly define fzf-native as a dependency
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+      },
+      "nvim-telescope/telescope-ui-select.nvim",
+    },
+    config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+
+      telescope.setup({
+        defaults = {
+          layout_strategy = "horizontal",
+          layout_config = {
+            prompt_position = "top",
+          },
+          sorting_strategy = "ascending",
+        },
+        pickers = {
+          buffers = {
+            show_all_buffers = true,
+            sort_lastused = true,
+            theme = "dropdown",
+            previewer = false,
+            mappings = {
+              i = {
+                ["<c-d>"] = "delete_buffer",
+              },
+            },
+          },
+          find_files = {
+            hidden = true,
+            no_ignore = true,
+            file_ignore_patterns = { ".git/" },
+          },
+        },
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+          -- Correctly configure fzf
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter = true,
+            case_mode = "smart_case",
+          },
+        },
+      })
+      -- Load extensions after setup
+      telescope.load_extension("ui-select")
+      telescope.load_extension("fzf")
+    end,
+  },
 }
