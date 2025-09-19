e2948b4f38e313a8309f885bd73866e6d239404e
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2025-09-19 13:59:07 +0200

    update

---
+++ nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,9 @@
+return {
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you prefer nvim-web-devicons
+	opts = {
+		file_types = { "markdown", "rmd", "Rmd", "jmd", "Jmd", "quarto" },
+		render_modes = { "n", "c", "t" },
+		anti_conceal = { enabled = false },
+	},
+}
