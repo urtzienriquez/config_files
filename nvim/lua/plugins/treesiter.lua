d290081ebd9c4443ccdba1dc59982b67f39534e7
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2025-10-27 21:33:07 +0100

    update

--- nvim/lua/plugins/treesiter.lua
+++ nvim/lua/plugins/treesiter.lua
@@ -1,12 +1,16 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
 		branch = "main",
+		lazy = false,
+		build = ":TSUpdate",
 		config = function()
-			local treesitter = require("nvim-treesitter")
-			treesitter.setup({})
-			local should_install = {
+			local ts = require("nvim-treesitter")
+
+			-- optional: change install dir, etc.
+			ts.setup({})
+
+			local parsers = {
 				"bash",
 				"c",
 				"css",
@@ -31,30 +35,28 @@ return {
 				"regex",
 			}
 
-			local installed = treesitter.get_installed()
-			local to_install = {}
-			for _, lang in ipairs(should_install) do
-				if not vim.tbl_contains(installed, lang) then
-					table.insert(to_install, lang)
-				end
-			end
-			treesitter.install(to_install)
+			-- Install asynchronously (or add :wait for sync bootstrap)
+			ts.install(parsers)
 
+			-- Start treesitter highlighting when the language is available
 			vim.api.nvim_create_autocmd("FileType", {
 				callback = function(args)
-					if vim.list_contains(treesitter.get_installed(), vim.treesitter.language.get_lang(args.match)) then
-						vim.treesitter.start(args.buf)
+					local lang = vim.treesitter.language.get_lang(args.match)
+					if not lang then
+						return
 					end
+
+					-- Safely attempt to start treesitter; if parser missing, no error
+					pcall(vim.treesitter.start, args.buf)
 				end,
 			})
 		end,
 	},
+
 	{
 		"nvim-treesitter/nvim-treesitter-textobjects",
 		branch = "main",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-		},
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
 		config = function()
 			require("nvim-treesitter-textobjects").setup({
 				select = {
