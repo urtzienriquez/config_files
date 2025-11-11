df17ef45a0c24e32d7516079cab47bebc3e4036a
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2025-11-11 16:10:36 +0100

    update

--- qtile/screensc.py
+++ qtile/screensc.py
@@ -1,6 +1,6 @@
 from libqtile.config import Screen
 
 screens = [
-    Screen(wallpaper="~/Pictures/linux-wallpaper.png", wallpaper_mode="fill"),
-    Screen(wallpaper="~/Pictures/linux-wallpaper.png", wallpaper_mode="fill"),
+    Screen(wallpaper="~/Pictures/tux.png", wallpaper_mode="fill"),
+    Screen(wallpaper="~/Pictures/tux.png", wallpaper_mode="fill"),
 ]
