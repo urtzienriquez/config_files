e8a93fc3ed6938b7bc1d645fc8b467b51c4479d8
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2025-11-14 05:43:00 +0100

    update

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -34,11 +34,6 @@ return {
 					{ "branch", icon = "󰘬", },
 					{ "diff"},
 					{ "diagnostics"},
-					{
-						require("noice").api.statusline.mode.get,
-						cond = require("noice").api.statusline.mode.has,
-						color = { gui = "bold" },
-					},
 				},
 				lualine_x = { "filetype" },
 				lualine_y = {},
