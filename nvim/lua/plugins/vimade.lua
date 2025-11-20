50324c3f85d4704c9d2a1267f4b17556ca081b7e
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2025-11-20 19:04:59 +0100

    update

---
+++ nvim/lua/plugins/vimade.lua
@@ -0,0 +1,11 @@
+return {
+	"tadaa/vimade",
+	opts = {
+		recipe = { "minimalist", { animate = true } },
+		fadelevel = 0.4,
+		tint = {
+			fg = { rgb = { 255, 0, 0 }, intensity = 0.5 },
+			bg = { rgb = { 0, 0, 0 }, intensity = 0.2 },
+		},
+	},
+}
