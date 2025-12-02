41a091357cadf427c170b97761bf6d80b5c39f71
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2025-12-02 19:01:25 +0100

    update

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,4 +1,3 @@
--- nvim/lua/plugins/noice.lua
 return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
@@ -8,30 +7,26 @@ return {
 	opts = {
 		cmdline = {
 			enabled = true,
-			view = "cmdline", -- Use classic cmdline view
+			view = "cmdline",
 			format = {
 				cmdline = false,
 				search_up = false,
 				search_down = false,
 				filter = false,
-				-- input = false,
+				input = {
+					view = "cmdline",
+				},
 				lua = false,
 				help = false,
 			},
 		},
 		messages = {
 			enabled = true,
-			view = "mini", -- Use minimal message view
+			view = "mini",
 		},
 		lsp = {
-			override = {
-				-- Override markdown rendering for hover docs
-				["vim.lsp.util.convert_input_to_markdown_lines"] = false,
-				["vim.lsp.util.stylize_markdown"] = false,
-				["cmp.entry.get_documentation"] = false,
-			},
 			signature = {
-				enabled = false, -- Disable if it causes issues
+				enabled = false,
 			},
 		},
 		presets = {
@@ -39,30 +34,7 @@ return {
 			command_palette = false,
 			long_message_to_split = false,
 			inc_rename = false,
-			lsp_doc_border = false,
-		},
-		routes = {
-			{
-				-- Route long messages to split
-				filter = {
-					event = "msg_show",
-					min_height = 10,
-				},
-				view = "split",
-			},
-			{
-				-- Skip certain annoying messages
-				filter = {
-					event = "msg_show",
-					any = {
-						{ find = "%d+L, %d+B" },
-						{ find = "; after #%d+" },
-						{ find = "; before #%d+" },
-						{ find = "fewer lines" },
-					},
-				},
-				opts = { skip = true },
-			},
+			lsp_doc_border = true,
 		},
 	},
 }
