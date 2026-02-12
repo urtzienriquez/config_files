a99b72a425cbdba896b90ee84adc3d7f93d3ba4d
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-02-12 18:18:15 +0100

    update

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -6,24 +6,6 @@ return {
 		{ "<leader>gp", "<cmd>Git pull<cr>", mode = "n", desc = "Git pull" },
 	},
 	config = function()
-		local My_Fugitive = vim.api.nvim_create_augroup("My_Fugitive", {})
-
-		local autocmd = vim.api.nvim_create_autocmd
-		autocmd("BufWinEnter", {
-			group = My_Fugitive,
-			pattern = "*",
-			callback = function()
-				if vim.bo.ft ~= "fugitive" then
-					return
-				end
-
-				local bufnr = vim.api.nvim_get_current_buf()
-				local opts = { buffer = bufnr, remap = false }
-				vim.keymap.set("n", "S", function()
-					vim.cmd.Git("add -A")
-				end, opts)
-			end,
-		})
 		-- Set up keymaps
 		vim.keymap.set("n", "<leader>gc", "<cmd>Git commit<cr>", { desc = "Git commit" })
 		vim.keymap.set("n", "<leader>gl", "<cmd>Git log<cr>", { desc = "Git log" })
