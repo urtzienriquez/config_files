c1d2bb1a4b4d0b897a6d3dce2720dd1b6954646b
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-02-12 22:12:56 +0100

    update

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -5,13 +5,13 @@ return {
 		{ "<leader>gs", "<cmd>Git<cr>", mode = "n", desc = "Git status" },
 		{ "<leader>gp", "<cmd>Git pull<cr>", mode = "n", desc = "Git pull" },
 		{ "<leader>gl", "<cmd>Git log<cr>", mode = "n", desc = "Git log" },
+		{ "<leader>gB", "<cmd>Git blame<cr>", mode = "n", desc = "Git blame" },
 	},
 	config = function()
 		-- Set up keymaps
 		vim.keymap.set("n", "<leader>gc", "<cmd>Git commit<cr>", { desc = "Git commit" })
 		vim.keymap.set("n", "<leader>gP", "<cmd>Git push<cr>", { desc = "Git push" })
 		vim.keymap.set("n", "<leader>gv", "<cmd>Gvdiffsplit!<cr>", { desc = "Git diff split" })
-		vim.keymap.set("n", "<leader>gB", "<cmd>Git blame<cr>", { desc = "Git blame" })
 		vim.keymap.set("n", "<leader>gw", "<cmd>Gwrite<cr>", { desc = "Git write (stage current file)" })
 		vim.keymap.set("n", "<leader>gr", "<cmd>Gread<cr>", { desc = "Git read (checkout current file)" })
 	end,
