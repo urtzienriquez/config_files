736dd2f52577cf9a0dc48f3b09746505c0805b76
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-02-13 10:31:07 +0100

    update

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -1,15 +1,20 @@
 return {
 	"tpope/vim-fugitive",
-	cmd = { "G", "Git" },
-	keys = {
-		{ "<leader>gs", "<cmd>Git<cr>", mode = "n", desc = "Git status" },
-		{ "<leader>gf", "<cmd>Git fetch<cr>", mode = "n", desc = "Git pull" },
-		{ "<leader>gp", "<cmd>Git pull<cr>", mode = "n", desc = "Git pull" },
-		{ "<leader>gl", "<cmd>Git log<cr>", mode = "n", desc = "Git log" },
-		{ "<leader>gB", "<cmd>Git blame<cr>", mode = "n", desc = "Git blame" },
-	},
+	-- cmd = { "G", "Git" },
+	-- keys = {
+	-- 	{ "<leader>gs", "<cmd>Git<cr>", mode = "n", desc = "Git status" },
+	-- 	{ "<leader>gf", "<cmd>Git fetch<cr>", mode = "n", desc = "Git pull" },
+	-- 	{ "<leader>gp", "<cmd>Git pull<cr>", mode = "n", desc = "Git pull" },
+	-- 	{ "<leader>gl", "<cmd>Git log<cr>", mode = "n", desc = "Git log" },
+	-- 	{ "<leader>gB", "<cmd>Git blame<cr>", mode = "n", desc = "Git blame" },
+	-- },
 	config = function()
 		-- Set up keymaps
+		vim.keymap.set("n", "<leader>gs", "<cmd>Git<cr>", { desc = "Git status" })
+		vim.keymap.set("n", "<leader>gf", "<cmd>Git fetch<cr>", { desc = "Git fetch" })
+		vim.keymap.set("n", "<leader>gp", "<cmd>Git pull<cr>", { desc = "Git pull" })
+		vim.keymap.set("n", "<leader>gl", "<cmd>Git log<cr>", { desc = "Git log" })
+		vim.keymap.set("n", "<leader>gB", "<cmd>Git blame<cr>", { desc = "Git blame" })
 		vim.keymap.set("n", "<leader>gc", "<cmd>Git commit<cr>", { desc = "Git commit" })
 		vim.keymap.set("n", "<leader>gP", "<cmd>Git push<cr>", { desc = "Git push" })
 		vim.keymap.set("n", "<leader>gv", "<cmd>Gvdiffsplit!<cr>", { desc = "Git diff split" })
--- nvim/lua/plugins/vim-tmux-nav.lua
+++ nvim/lua/plugins/vim-tmux-nav.lua
@@ -1,12 +1,18 @@
 return {
-	"christoomey/vim-tmux-navigator",
-	lazy = false, -- Changed to load immediately so commands are available
-	cmd = {
-		"TmuxNavigateLeft",
-		"TmuxNavigateDown",
-		"TmuxNavigateUp",
-		"TmuxNavigateRight",
-		"TmuxNavigatePrevious",
-		"TmuxNavigatorProcessList",
-	},
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+    "TmuxNavigatorProcessList",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
 }
