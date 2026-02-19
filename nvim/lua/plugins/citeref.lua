117b8027f9972ec434b240cbe8ad119a6ea036c5
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-02-19 19:25:24 +0100

    update

--- nvim/lua/plugins/citeref.lua
+++ nvim/lua/plugins/citeref.lua
@@ -1,6 +1,7 @@
 return {
 	"urtzienriquez/citeref.nvim",
-    dev = false,
+	dev = false,
+	ft = { "markdown", "rmd", "quarto", "rnoweb", "pandoc", "tex", "latex" },
 	dependencies = { "ibhagwan/fzf-lua" },
 	config = function()
 		require("citeref").setup({
