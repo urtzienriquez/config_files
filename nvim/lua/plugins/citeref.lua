7cc6fcde494007834cf60e4967e6a2f025d18118
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-02-20 02:42:53 +0100

    add backend for citeref

--- nvim/lua/plugins/citeref.lua
+++ nvim/lua/plugins/citeref.lua
@@ -4,6 +4,7 @@ return {
 	ft = { "markdown", "rmd", "quarto", "rnoweb", "pandoc", "tex", "latex" },
 	config = function()
 		require("citeref").setup({
+			backend = "fzf",
 			bib_files = { "~/Documents/zotero.bib" },
 		})
 	end,
