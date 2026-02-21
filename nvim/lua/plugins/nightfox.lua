c6dc1879cc73ec4a92abe9f8dca39e6f30e479ad
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-02-21 12:36:52 +0100

    rm commented code from nightfox.lua

--- nvim/lua/plugins/nightfox.lua
+++ nvim/lua/plugins/nightfox.lua
@@ -2,19 +2,6 @@ return {
 	"urtzienriquez/nightfox.nvim",
     dev = true,
 	config = function()
-		-- require("nightfox").setup({
-		--           dim_inactive = true, -- defaults to 0.4
-		-- 	on_load = function(spec, palette)
-		-- 		-- highlight overrides
-		-- 		vim.api.nvim_set_hl(0, "NormalNC", { fg = spec.fg1, bg = spec.bg1 })
-		-- 		vim.api.nvim_set_hl(0, "WinSeparator", { fg = palette.blue.base, bg = "none" })
-		--
-		-- 		-- style overrides
-		-- 		vim.api.nvim_set_hl(0, "Comment", { fg = spec.syntax.comment, italic = true })
-		-- 		vim.api.nvim_set_hl(0, "Keyword", { fg = spec.syntax.keyword, bold = true })
-		-- 		vim.api.nvim_set_hl(0, "@markup.strong", { bold = true })
-		-- 	end,
-		-- })
 		vim.cmd.colorscheme("nightfox")
 	end,
 }
