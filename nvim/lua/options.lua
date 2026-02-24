c4cc79732f181253c32d1f0f6bbae5f8e5aad8b1
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-02-24 07:32:32 +0100

    add lsp message in ui2

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -173,3 +173,21 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.keymap.del("n", "q", { buffer = true })
 	end,
 })
+
+-- lsp message in ui2
+vim.api.nvim_create_autocmd("LspProgress", {
+  callback = function(ev)
+    local value = ev.data.params.value or {}
+    local msg = value.message or "lsp: done"
+    if #msg > 40 then
+      msg = msg:sub(1, 37) .. "..."
+    end
+    vim.api.nvim_echo({ { msg } }, false, {
+      id = "lsp",
+      kind = "progress",
+      title = value.title,
+      status = value.kind ~= "end" and "running" or "success",
+      percent = value.percentage,
+    })
+  end,
+})
