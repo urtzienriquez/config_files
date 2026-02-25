4e27fd3a7fd7429d9c122e617f0989cdb28f7146
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-02-25 12:05:00 +0100

    simplify blink config

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -15,7 +15,6 @@ return {
 			use_nvim_cmp_as_default = true,
 			nerd_font_variant = "mono",
 		},
-
 		sources = {
 			default = { "lsp", "path", "snippets", "buffer" },
 			providers = {
@@ -34,38 +33,24 @@ return {
 				tex = { inherit_defaults = true, "citeref" },
 			},
 		},
-
 		completion = {
 			list = { selection = { preselect = true, auto_insert = true } },
 			accept = { auto_brackets = { enabled = false } },
-
 			menu = {
-				border = "none",
-				winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None",
-
 				draw = {
-					columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "source_name", gap = 1 } },
+					columns = { { "label", gap = 1 }, { "kind_icon", "source_name", gap = 1 } },
 				},
 			},
-
 			documentation = {
-				auto_show = false,
+				auto_show = true,
 				treesitter_highlighting = true,
-				window = {
-					border = "none",
-					winhighlight = "Normal:CmpDoc,Search:None",
-				},
 			},
-
 			ghost_text = { enabled = false },
 		},
-
 		signature = {
 			enabled = true,
 			window = {
-				show_documentation = false,
-				border = "none",
-				winhighlight = "Normal:CmpSignatureHelp",
+				show_documentation = true,
 			},
 		},
 	},
