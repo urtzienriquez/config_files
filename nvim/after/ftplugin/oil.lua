30af89e792dcd835faaac360592211ecb53ef0ab
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-03-03 02:17:24 +0100

    delete and hide from whichkey unneeded keymaps in oil buffers

--- nvim/after/ftplugin/oil.lua
+++ nvim/after/ftplugin/oil.lua
@@ -1,1 +1,22 @@
 vim.b.completion = false
+
+vim.schedule(function()
+  local buf = vim.api.nvim_get_current_buf()
+  local wk = require("which-key")
+
+  local noop = function(mode, lhs)
+    vim.keymap.set(mode, lhs, "<Nop>", { buffer = buf, silent = true })
+  end
+
+  noop("n", "<leader>rr")
+  noop("n", "<leader><Enter>")
+  noop("v", "<leader><Enter>")
+  noop("n", "<leader><leader><Enter>")
+
+  -- Hide from which-key in this buffer
+  wk.add({
+    { "<leader>rr", hidden = true, buffer = buf },
+    { "<leader><CR>", hidden = true, buffer = buf },
+    { "<leader><leader><CR>", hidden = true, buffer = buf },
+  })
+end)
