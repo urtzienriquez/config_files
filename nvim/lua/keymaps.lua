719ace0e3d8f4fd50f952bfa29eba1d390719814
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-03-04 22:24:37 +0100

    fix * behaviour

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -14,7 +14,11 @@ vim.keymap.set("n", "<Left>", "<cmd>cclose<CR>", { noremap = true, silent = true
 vim.keymap.set("n", "<Right>", "<cmd>copen<CR>", { noremap = true, silent = true })
 
 -- Highlight without moving
-vim.keymap.set("n", "*", "*``")
+vim.keymap.set("n", "*", function()
+  local word = vim.fn.expand("<cword>")
+  vim.fn.setreg("/", word)
+  vim.opt.hlsearch = true
+end, { noremap = true, desc = "Search word under cursor, stay in place" })
 
 -- Escape terminal mode
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
