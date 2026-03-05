2e06a8e0e8b92036822c30c782424a79eb31bd87
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-03-05 03:02:58 +0100

    add keymap to run plenary tests in _spec.lua files

--- nvim/lua/plugins/plenary.lua
+++ nvim/lua/plugins/plenary.lua
@@ -1,1 +1,17 @@
-return { "nvim-lua/plenary.nvim" }
+return {
+  "nvim-lua/plenary.nvim",
+  init = function()
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+      pattern = "*_spec.lua",
+      callback = function(event)
+        require("plenary")
+        vim.keymap.set("n", "<leader>rt", function()
+          vim.cmd("PlenaryBustedFile %")
+        end, {
+          buffer = event.buf,
+          desc = "run tests plenary",
+        })
+      end,
+    })
+  end,
+}
