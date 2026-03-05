04b735c85886607e6c476cc8fe9dd120737567c4
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-03-05 03:28:06 +0100

    fix winborder in plenary tests

--- nvim/lua/plugins/plenary.lua
+++ nvim/lua/plugins/plenary.lua
@@ -6,7 +6,10 @@ return {
       callback = function(event)
         require("plenary")
         vim.keymap.set("n", "<leader>rt", function()
+          local prev = vim.o.winborder
+          vim.o.winborder = "none"
           vim.cmd("PlenaryBustedFile %")
+          vim.o.winborder = prev
         end, {
           buffer = event.buf,
           desc = "run tests plenary",
