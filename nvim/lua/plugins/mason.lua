82550e8ae1653352f37d91d4128bec745ba58964
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-03-10 09:16:10 +0100

    mason on startup

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,6 +1,5 @@
 vim.pack.add({ "https://github.com/mason-org/mason.nvim" }, { load = false })
 
-vim.api.nvim_create_user_command("Mason", function(cmd_opts)
   require("mason").setup({
     ui = {
       border = "none",
@@ -8,5 +7,3 @@ vim.api.nvim_create_user_command("Mason", function(cmd_opts)
       icons = { package_installed = "✓", package_pending = "➜", package_uninstalled = "✗" },
     },
   })
-  vim.cmd("Mason " .. (cmd_opts.args or ""))
-end, { nargs = "*" })
