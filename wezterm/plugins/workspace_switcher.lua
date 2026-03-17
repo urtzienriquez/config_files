19aed7625ccc4cb68cf66c52fc4203fffc3af861
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-03-17 01:04:20 +0100

    fix(workspace_switcher): bad naming for the theme was causing the plugin not to load

--- wezterm/plugins/workspace_switcher.lua
+++ wezterm/plugins/workspace_switcher.lua
@@ -9,10 +9,9 @@ function mod.with_options(config)
   workspace_switcher.apply_to_config(config)
 
   workspace_switcher.workspace_formatter = function(label)
-    -- Fallback to nightfox if global isn't ready, but current_theme
-    -- is initialized in colors.lua immediately upon require.
-    local theme = _G.current_theme or "nightfox"
-    local active_colors = colors.tab_bar_configs[theme].active_tab
+    local theme_name = _G.current_theme or "nightfox"
+    local theme = colors.themes[theme_name]
+    local active_colors = theme.tab_bar.active_tab
 
     return wezterm.format({
       { Attribute = { Intensity = "Bold" } },
