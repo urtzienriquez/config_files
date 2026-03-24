fa9cfa14b2cb0cae32cf4fe5d42c04ca8bf75cdb
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-03-24 13:47:31 +0100

    (nvim) rm r.lua in ftplugin -- allow language server to highlight colors with color background

--- nvim/after/ftplugin/r.lua
+++
@@ -1,9 +0,0 @@
-  vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup("LspColors", { clear = true }),
-    callback = function(args)
-      local client = vim.lsp.get_client_by_id(args.data.client_id)
-      if client then
-        vim.lsp.document_color.enable(false, args.buf)
-      end
-    end,
-  })
