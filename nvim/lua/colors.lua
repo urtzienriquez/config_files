9a7596128a988b32bb5678aa95c3efddec2f465a
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-04-12 21:47:49 +0200

    some color overrides

---
+++ nvim/lua/colors.lua
@@ -0,0 +1,28 @@
+local set = vim.api.nvim_set_hl
+
+set(0, "@markup.heading.1.markdown", { link = "WarningMsg", bold = true })
+set(0, "@markup.heading.2.markdown", { link = "WarningMsg", bold = true })
+set(0, "@markup.heading.3.markdown", { link = "WarningMsg", bold = true })
+set(0, "@markup.heading.4.markdown", { link = "WarningMsg" })
+set(0, "@markup.heading.5.markdown", { link = "WarningMsg" })
+set(0, "@markup.heading.6.markdown", { link = "WarningMsg" })
+set(0, "@markup.math.latex", { link = "Function" })
+set(0, "@operator.latex", { link = "Function" })
+set(0, "texOnlyMath", { link = "Function" })
+
+
+set(0, "NormalFloat", { link = "Normal" })
+set(0, "BlinkCmpMenu", { link = "Normal" })
+set(0, "BlinkCmpMenuBorder", { link = "Normal" })
+
+
+set(0, "Statement", { link = "WarningMsg" })
+set(0, "pandocCiteKey", { link = "Special", underline = false})
+set(0, "pandocCiteAnchor", { link = "Special" })
+set(0, "pandocCiteLocator", { link = "Special" })
+set(0, "pandocPCite", { link = "Special" })
+set(0, "texRefZone", { link = "Special" })
+set(0, "texStatement", { link = "Special" })
+set(0, "texDelimiter", { link = "Special" })
+set(0, "markdownCode", { link = "Special" })
+
