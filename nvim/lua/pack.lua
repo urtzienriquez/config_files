8df9a8ddb8980ec41a6ce22894c6182a5ba14fb9
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-04-14 00:16:20 +0200

    (statusline) don't show mode and recording macro with cmdheight=1

--- nvim/lua/pack.lua
+++ nvim/lua/pack.lua
@@ -77,30 +77,30 @@ require("nvim-web-devicons").setup({})
 require("mini.statusline").setup({
   content = {
     active = function()
-      local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+      local _, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
       local git = MiniStatusline.section_git({ trunc_width = 40 })
       local diff = MiniStatusline.section_diff({ trunc_width = 75 })
       local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
       local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
-      local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+      local filename = MiniStatusline.section_filename({ trunc_width = 999 })
       local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 999 })
       local location = MiniStatusline.section_location({ trunc_width = 75 })
       local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
-      local macro = ""
-      local reg = vim.fn.reg_recording()
-      if reg ~= "" then
-        macro = "recording @" .. reg
-      end
+      -- local macro = ""
+      -- local reg = vim.fn.reg_recording()
+      -- if reg ~= "" then
+      --   macro = "recording @" .. reg
+      -- end
 
       return MiniStatusline.combine_groups({
-        { hl = mode_hl, strings = { mode } },
-        { hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp } },
+        -- { hl = mode_hl, strings = { mode } },
+        { hl = "MiniStatuslineFileinfo", strings = { filename } },
+        { hl = "MiniStatuslineFilename", strings = { git, diff, diagnostics, lsp } },
         "%<",
-        { hl = "MiniStatuslineFilename", strings = { filename } },
         "%=",
-        { hl = "MiniStatuslineMode", strings = { macro } },
-        { hl = "MiniStatuslineFilename", strings = { fileinfo } },
-        { hl = mode_hl, strings = { search, location } },
+        -- { hl = "MiniStatuslineMode", strings = { macro } },
+        { strings = { fileinfo } },
+        { hl = "MiniStatuslineFileinfo", strings = { search, location } },
       })
     end,
   },
