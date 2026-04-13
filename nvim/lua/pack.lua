ed3849d6540281eaaa63c62bae5cb6dc63c80fc2
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-04-13 13:09:55 +0200

    add lsp info to mini.statusline

--- nvim/lua/pack.lua
+++ nvim/lua/pack.lua
@@ -81,6 +81,7 @@ require("mini.statusline").setup({
       local git = MiniStatusline.section_git({ trunc_width = 40 })
       local diff = MiniStatusline.section_diff({ trunc_width = 75 })
       local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+      local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
       local filename = MiniStatusline.section_filename({ trunc_width = 140 })
       local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 999 })
       local location = MiniStatusline.section_location({ trunc_width = 75 })
@@ -93,7 +94,7 @@ require("mini.statusline").setup({
 
       return MiniStatusline.combine_groups({
         { hl = mode_hl, strings = { mode } },
-        { hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics } },
+        { hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp } },
         "%<",
         { hl = "MiniStatuslineFilename", strings = { filename } },
         "%=",
