d5787d934f68f416b90dc4bdd18d0d6ce12bf00b
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-04-17 18:28:48 +0200

    change config (xrandr) of external monitor

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -7,4 +7,4 @@ unclutter --timeout 10 &
 xss-lock -- "/home/urtzi/.config/qtile/smartlock.sh" &
 calcurse --daemon &
 autorandr --change
-xrandr --output HDMI-1 --gamma 0.8:0.8:0.8 --brightness 0.8
+xrandr --output HDMI-1 --gamma 0.9:0.9:0.9 --brightness 0.9
