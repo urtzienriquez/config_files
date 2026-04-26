65a0bfb43367d5ea3d966bf5a1c3dc712210d00f
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-04-26 21:43:03 +0200

    reorganize pack.lua file

--- nvim/lua/pack.lua
+++ nvim/lua/pack.lua
@@ -1,11 +1,4 @@
--- BUILTIN plugins that require loading
-
--- undotree
-vim.cmd("packadd nvim.undotree")
-
--- EXTERNAL plugins
-
--- Build hooks (must be before vim.pack.add)
+-- hooks
 vim.api.nvim_create_autocmd("PackChanged", {
   callback = function(ev)
     local name, kind = ev.data.spec.name, ev.data.kind
@@ -20,6 +13,15 @@ vim.api.nvim_create_autocmd("PackChanged", {
     end
   end,
 })
+
+----------------------------------------
+-- builtin plugins that require loading
+
+-- undotree
+vim.cmd("packadd nvim.undotree")
+
+----------------------------------------
+-- external plugins
 
 -- helper
 local gh = function(x)
@@ -54,7 +56,23 @@ vim.pack.add({
   gh("jpalardy/vim-slime"),
 }, { load = false })
 
--- CONFIGURATION
+----------------------------------------
+-- my plugins
+
+local dev = vim.fn.expand("~/Documents/GitHub")
+local my_packs = {
+  "nightfox.nvim",
+  "citeref.nvim",
+  "replent.nvim",
+  "sessman.nvim",
+  "learnlua.nvim",
+}
+for _, name in ipairs(my_packs) do
+  vim.opt.rtp:prepend(dev .. "/" .. name)
+end
+
+----------------------------------------
+-- configuration
 
 -- which-key
 require("which-key").setup({
@@ -420,75 +438,6 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   end,
 })
 
--- R.nvim
-local function set_rnvim_keymaps()
-  local o = { noremap = true, silent = true, buffer = true }
-  vim.keymap.set("n", "<leader>or", "<Plug>RStart", o)
-  vim.keymap.set("n", "<leader>qr", "<Plug>RClose", o)
-  vim.keymap.set("n", "<leader>cd", "<Plug>RSetwd", o)
-  vim.keymap.set("n", "<Enter>", "<Plug>RDSendLine", o)
-  vim.keymap.set("v", "<Enter>", "<Plug>RSendSelection", o)
-  vim.keymap.set("n", "<leader>sb", "<Plug>RSendFile", o)
-  vim.keymap.set("n", "<leader>rh", "<Plug>RHelp", o)
-  vim.keymap.set("n", "<leader>ro", "<Plug>ROBToggle", o)
-  vim.keymap.set("n", "]]", "<Plug>RNextRChunk", o)
-  vim.keymap.set("n", "[[", "<Plug>RPreviousRChunk", o)
-  vim.keymap.set("i", "<C-a>c", "`r<Space>`<Esc>i", o)
-  vim.keymap.set("n", "<leader>ac", "i`r<Space>`<Esc>i", vim.tbl_extend("force", o, { desc = "Add inline code" }))
-  if vim.bo.filetype == "rmd" then
-    vim.keymap.set("n", "<leader>rr", function()
-      local filename = vim.fn.input({ prompt = "Output filename (without extension): ", cancelreturn = "__CANCEL__" })
-      vim.api.nvim_echo({ { "" } }, false, {})
-      if filename == "__CANCEL__" then
-        return
-      end
-      vim.cmd('RSend if(exists("params")) rm(params)')
-      local file = vim.fn.expand("%")
-      if filename ~= "" then
-        vim.cmd('RSend rmarkdown::render("' .. file .. '", output_file = "' .. filename .. '")')
-      else
-        vim.cmd('RSend rmarkdown::render("' .. file .. '")')
-      end
-    end, { desc = "Render R Markdown" })
-  end
-end
-
-local r_opts = {
-  R_app = "R",
-  external_term = "tmux split-window -d -h",
-  bracketed_paste = false,
-  R_args = { "--no-save --silent" },
-  user_maps_only = true,
-  objbr_mappings = {
-    c = "class",
-    ["<leader>gp"] = "head({object}, n = 15)",
-    v = function()
-      require("r.browser").toggle_view()
-    end,
-  },
-  hook = {
-    on_filetype = function()
-      if vim.bo.filetype ~= "quarto" then
-        set_rnvim_keymaps()
-        return
-      end
-      for _, line in ipairs(vim.api.nvim_buf_get_lines(0, 0, 100, false)) do
-        local lang = line:match("^```{(%w+)")
-        if lang and lang:lower() == "r" then
-          set_rnvim_keymaps()
-          return
-        end
-      end
-    end,
-  },
-}
-if vim.env.R_AUTO_START == "true" then
-  r_opts.auto_start = "on startup"
-  r_opts.objbr_auto_start = true
-end
-
-require("r").setup(r_opts)
-
 -- blink.cmp
 vim.api.nvim_create_autocmd({ "InsertEnter", "CmdlineEnter" }, {
   once = true,
@@ -628,8 +577,76 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
--- opencode.nvim
+-- R.nvim
+local function set_rnvim_keymaps()
+  local o = { noremap = true, silent = true, buffer = true }
+  vim.keymap.set("n", "<leader>or", "<Plug>RStart", o)
+  vim.keymap.set("n", "<leader>qr", "<Plug>RClose", o)
+  vim.keymap.set("n", "<leader>cd", "<Plug>RSetwd", o)
+  vim.keymap.set("n", "<Enter>", "<Plug>RDSendLine", o)
+  vim.keymap.set("v", "<Enter>", "<Plug>RSendSelection", o)
+  vim.keymap.set("n", "<leader>sb", "<Plug>RSendFile", o)
+  vim.keymap.set("n", "<leader>rh", "<Plug>RHelp", o)
+  vim.keymap.set("n", "<leader>ro", "<Plug>ROBToggle", o)
+  vim.keymap.set("n", "]]", "<Plug>RNextRChunk", o)
+  vim.keymap.set("n", "[[", "<Plug>RPreviousRChunk", o)
+  vim.keymap.set("i", "<C-a>c", "`r<Space>`<Esc>i", o)
+  vim.keymap.set("n", "<leader>ac", "i`r<Space>`<Esc>i", vim.tbl_extend("force", o, { desc = "Add inline code" }))
+  if vim.bo.filetype == "rmd" then
+    vim.keymap.set("n", "<leader>rr", function()
+      local filename = vim.fn.input({ prompt = "Output filename (without extension): ", cancelreturn = "__CANCEL__" })
+      vim.api.nvim_echo({ { "" } }, false, {})
+      if filename == "__CANCEL__" then
+        return
+      end
+      vim.cmd('RSend if(exists("params")) rm(params)')
+      local file = vim.fn.expand("%")
+      if filename ~= "" then
+        vim.cmd('RSend rmarkdown::render("' .. file .. '", output_file = "' .. filename .. '")')
+      else
+        vim.cmd('RSend rmarkdown::render("' .. file .. '")')
+      end
+    end, { desc = "Render R Markdown" })
+  end
+end
 
+local r_opts = {
+  R_app = "R",
+  external_term = "tmux split-window -d -h",
+  bracketed_paste = false,
+  R_args = { "--no-save --silent" },
+  user_maps_only = true,
+  objbr_mappings = {
+    c = "class",
+    ["<leader>gp"] = "head({object}, n = 15)",
+    v = function()
+      require("r.browser").toggle_view()
+    end,
+  },
+  hook = {
+    on_filetype = function()
+      if vim.bo.filetype ~= "quarto" then
+        set_rnvim_keymaps()
+        return
+      end
+      for _, line in ipairs(vim.api.nvim_buf_get_lines(0, 0, 100, false)) do
+        local lang = line:match("^```{(%w+)")
+        if lang and lang:lower() == "r" then
+          set_rnvim_keymaps()
+          return
+        end
+      end
+    end,
+  },
+}
+if vim.env.R_AUTO_START == "true" then
+  r_opts.auto_start = "on startup"
+  r_opts.objbr_auto_start = true
+end
+
+require("r").setup(r_opts)
+
+-- opencode.nvim
 ---@diagnostic disable: undefined-field
 
 vim.keymap.set("n", "<leader>io", function()
@@ -651,19 +668,7 @@ end, { desc = "Add range to opencode", expr = true })
 
 ---@diagnostic enable: undefined-field
 
--- my plugins
-
-local dev = vim.fn.expand("~/Documents/GitHub")
-local my_packs = {
-  "nightfox.nvim",
-  "citeref.nvim",
-  "replent.nvim",
-  "learnlua.nvim",
-}
-for _, name in ipairs(my_packs) do
-  vim.opt.rtp:prepend(dev .. "/" .. name)
-end
-
+-- nightfox
 require("nightfox").setup()
 
 -- citeref
@@ -676,4 +681,9 @@ require("citeref").setup({
 require("replent").setup({
   strategy = "tmux",
   repl_commands = { python = "python" },
+})
+
+-- sessman
+require("sessman").setup({
+  backend = "fzf",
 })
