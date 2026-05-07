0d89dac35eae162290a315de29b595a7090d7024
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-05-07 17:06:27 +0200

    (nvim) add texlab lsp for latex documents

---
+++ nvim/lsp/texlab.lua
@@ -0,0 +1,59 @@
+return {
+  cmd = { vim.fn.stdpath("data") .. "/mason/bin/texlab" },
+  filetypes = { "tex", "plaintex", "bib" },
+  root_markers = {
+    ".latexmkrc",
+    ".texlabroot",
+    ".git",
+  },
+  settings = {
+    texlab = {
+      -- build = {
+      --   executable = "latexmk",
+      --   args = {
+      --     "-pdf",
+      --     "-interaction=nonstopmode",
+      --     "-synctex=1",
+      --     "%f",
+      --   },
+      --   onSave = true,
+      -- },
+      --   build = {
+      --     executable = "sh",
+      --     args = {
+      --       "-c",
+      --       [[
+      --   latexmk -pdf -interaction=nonstopmode -synctex=1 "$1" &&
+      --   latexmk -c "$1" &&
+      --   rm -f "${1%.tex}.synctex.gz"
+      -- ]],
+      --       "sh",
+      --       "%f",
+      --     },
+      --     onSave = true,
+      --   },
+      build = {
+        executable = "sh",
+        args = {
+          "-c",
+          [[
+      latexmk -pdf -interaction=nonstopmode -synctex=1 "$1" &&
+      latexmk -c "$1" &&
+      find . -maxdepth 1 -name "*.synctex.gz" -delete
+    ]],
+          "sh",
+          "%f",
+        },
+        onSave = true,
+      },
+      forwardSearch = {
+        executable = "zathura",
+        args = { "--synctex-forward", "%l:1:%f", "%p" },
+      },
+      chktex = {
+        onOpenAndSave = true,
+        onEdit = false,
+      },
+    },
+  },
+}
