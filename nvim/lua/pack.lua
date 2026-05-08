a5f536fae7416dca25226149afa316ecd2cdfdfa
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-05-08 15:00:14 +0200

    (nvim) delete open in horizontal/vertical actions from oil

--- nvim/lua/pack.lua
+++ nvim/lua/pack.lua
@@ -148,8 +148,6 @@ require("oil").setup({
     ["t"] = { "actions.parent", mode = "n" },
     ["<C-h>"] = false,
     ["<C-l>"] = false,
-    ["<C-s>"] = { "actions.select", opts = { horizontal = true }, desc = "Open in horizontal split" },
-    ["<C-v>"] = { "actions.select", opts = { vertical = true }, desc = "Open in vertical split" },
     ["<leader>l"] = "actions.refresh",
   },
 })
