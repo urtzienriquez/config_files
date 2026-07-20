b2062006f8938ddf08da920e88d1bdf29ed6c0e0
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-07-20 16:47:48 +0200

    (qtile) change background color of i3lock

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -3,7 +3,7 @@ xset s off
 xset -dpms
 xset s noblank
 unclutter --timeout 10 &
-xss-lock --transfer-sleep-lock -- i3lock -c 000000 --nofork &
+xss-lock --transfer-sleep-lock -- i3lock -c 192330 --nofork &
 /home/urtzi/.config/qtile/smartlock.sh &
 calcurse --daemon &
 autorandr --change
