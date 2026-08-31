e0326afc80879f0e42b6c7bf24a8798b170788ee
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-08-31 12:29:03 +0200

    (nvim) comment out laststatus = 3

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -5,7 +5,7 @@ vim.g.maplocalleader = "\\"
 -- misc options
 vim.o.mouse = ""
 vim.o.shell = "/usr/bin/zsh"
-vim.opt.laststatus = 3
+-- vim.opt.laststatus = 3
 vim.o.cmdheight = 0
 require("vim._core.ui2").enable({ msg = { targets = "msg" } })
 vim.o.winborder = "rounded"
