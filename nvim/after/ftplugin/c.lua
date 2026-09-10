34e84c94ede40a1c0d01bfc6670087d8e7e22245
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-09-10 13:05:59 +0200

    (nvim) add a keymap to compile c programs

--- nvim/after/ftplugin/c.lua
+++ nvim/after/ftplugin/c.lua
@@ -1,1 +1,7 @@
 vim.o.shiftwidth = 4
+
+vim.keymap.set("n", "<leader>cc", function()
+  vim.cmd("compiler gcc")
+  vim.opt.makeprg = "gcc -o %:r %"
+  vim.cmd("make")
+end, { desc = "compile c program" })
