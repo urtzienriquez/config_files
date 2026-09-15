35dff89a6500599316a6a82e61b900fdde9fc3a8
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-09-15 18:02:32 +0200

    (nvim) fix fzf-lua lazy loading

--- nvim/lua/pack.lua
+++ nvim/lua/pack.lua
@@ -282,7 +282,120 @@ require("gitsigns").setup({
 })
 
 -- fzf-lua (lazy: set up on first use)
-local fzf = require("lazy.fzf")
+local fzf = {}
+
+---@type fzf-lua?
+local fzf_lua = nil
+
+local function fzf_setup()
+  if fzf_lua then
+    return
+  end
+
+  require("nvim-web-devicons").setup({})
+
+  local actions = require("fzf-lua").actions
+
+  require("fzf-lua").setup({
+    defaults = { no_header_i = true, actions = { ["ctrl-q"] = actions.file_sel_to_qf } },
+    keymap = {
+      builtin = {
+        false,
+        ["<M-Esc>"] = "hide",
+        ["<F1>"] = "toggle-help",
+        ["<F2>"] = "toggle-fullscreen",
+        ["<F3>"] = "toggle-preview-wrap",
+        ["<F4>"] = "toggle-preview",
+        ["<F5>"] = "toggle-preview-cw",
+        ["<F6>"] = "toggle-preview-behavior",
+        ["<F7>"] = "toggle-preview-ts-ctx",
+        ["<F8>"] = "preview-ts-ctx-dec",
+        ["<F9>"] = "preview-ts-ctx-inc",
+        ["<S-Left>"] = "preview-reset",
+        ["<C-d>"] = "preview-down",
+        ["<C-u>"] = "preview-up",
+        ["ctrl-q"] = false,
+      },
+      fzf = {
+        false,
+        ["ctrl-u"] = false,
+        ["ctrl-z"] = "unix-line-discard+first",
+        ["ctrl-a"] = "toggle-all",
+        ["ctrl-r"] = "first",
+        ["ctrl-e"] = "last",
+        ["ctrl-q"] = false,
+      },
+    },
+    actions = {
+      files = {
+        ["enter"] = actions.file_edit_or_qf,
+        ["ctrl-s"] = actions.file_split,
+        ["ctrl-v"] = actions.file_vsplit,
+        ["ctrl-j"] = actions.toggle_ignore,
+        ["ctrl-h"] = actions.toggle_hidden,
+        ["ctrl-f"] = actions.toggle_follow,
+        ["ctrl-t"] = actions.buf_tabedit,
+      },
+    },
+    grep = { actions = { ["ctrl-f"] = { actions.grep_lgrep }, ["ctrl-g"] = false } },
+    buffers = { actions = { ["ctrl-x"] = { fn = actions.buf_del, reload = true } } },
+    fzf_opts = { ["--multi"] = true, ["--bind"] = "tab:toggle+down,shift-tab:toggle+up" },
+  })
+
+  fzf_lua = require("fzf-lua")
+
+  require("fzf-lua.providers.ui_select").register({
+    winopts = {
+      row = 0.5,
+      col = 0.5,
+      width = 0.5,
+      height = 0.5,
+      border = "rounded",
+    },
+  })
+end
+
+local function fzf_get()
+  if not fzf_lua then
+    fzf_setup()
+  end
+  if not fzf_lua then
+    error("fzf-lua failed to initialize")
+  end
+  return fzf_lua
+end
+
+local function fzf_call(name)
+  return function(...)
+    return fzf_get()[name](...)
+  end
+end
+
+fzf.builtin = fzf_call("builtin")
+fzf.files = fzf_call("files")
+fzf.zoxide = fzf_call("zoxide")
+fzf.live_grep_native = fzf_call("live_grep_native")
+fzf.grep_quickfix = fzf_call("grep_quickfix")
+fzf.buffers = fzf_call("buffers")
+fzf.help_tags = fzf_call("help_tags")
+fzf.keymaps = fzf_call("keymaps")
+fzf.grep_cword = fzf_call("grep_cword")
+fzf.diagnostics_document = fzf_call("diagnostics_document")
+fzf.diagnostics_workspace = fzf_call("diagnostics_workspace")
+fzf.lsp_definitions = fzf_call("lsp_definitions")
+fzf.lsp_references = fzf_call("lsp_references")
+fzf.lsp_document_symbols = fzf_call("lsp_document_symbols")
+fzf.treesitter = fzf_call("treesitter")
+fzf.spell_suggest = fzf_call("spell_suggest")
+fzf.marks = fzf_call("marks")
+fzf.resume = fzf_call("resume")
+fzf.oldfiles = fzf_call("oldfiles")
+fzf.git_branches = fzf_call("git_branches")
+fzf.git_commits = fzf_call("git_commits")
+
+fzf.home_files = function()
+  return (fzf_get().files({ cwd = vim.fn.expand("~"), prompt = "Home files❯ ", hidden = true }))
+end
 
 vim.keymap.set("n", "<leader>fp", fzf.builtin, { desc = "picker" })
 vim.keymap.set("n", "<leader>ff", fzf.files, { desc = "files" })
