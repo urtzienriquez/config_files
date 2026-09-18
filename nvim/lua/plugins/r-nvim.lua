3006d46d010c53ea68c556e418c9db7554255440
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-09-18 16:50:30 +0200

    (nvim) don't vim.notify when R console opens

--- nvim/lua/plugins/r-nvim.lua
+++ nvim/lua/plugins/r-nvim.lua
@@ -123,7 +123,6 @@ local r_opts = {
   },
   hook = {
     after_R_start = function()
-      vim.notify("R was launched")
       for _, win in ipairs(vim.api.nvim_list_wins()) do
         local buf = vim.api.nvim_win_get_buf(win)
         if vim.bo[buf].buftype == "terminal" and buf ~= vim.api.nvim_get_current_buf() then
