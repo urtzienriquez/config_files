f413723f2ef290edb3a23ee614e4be3da1469a09
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-09-18 19:49:44 +0200

    (nvim) fix diffs.nvim config

--- nvim/lua/plugins/diffs.lua
+++ nvim/lua/plugins/diffs.lua
@@ -3,8 +3,6 @@ vim.pack.add({ "https://github.com/barrettruth/diffs.nvim" })
 vim.g.diffs = {
   integrations = {
     fugitive = true,
-    neogit = true,
-    neojj = true,
     gitsigns = true,
   }
 }
