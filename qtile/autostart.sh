845bb740be26e3ebc1188941f76f78974a93bef8
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-09-19 04:46:34 +0200

    (qtile) add picom

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -3,6 +3,9 @@ xset s off
 xset -dpms
 xset s noblank
 unclutter --timeout 10 &
+pkill -x picom
+sleep 0.5
+picom -b --log-file "$HOME/.cache/picom.log" &
 xss-lock --transfer-sleep-lock -- i3lock -c 192330 --nofork &
 /home/urtzi/.config/qtile/smartlock.sh &
 calcurse --daemon &
