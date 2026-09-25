a8aa1da54a0afbe84e4690ad2b0b5946ae47a2f4
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-09-25 02:47:54 +0200

    (nvim) drop fzf picker from zotero.nvim config; not supported now

--- nvim/lua/plugins/zotero.lua
+++ nvim/lua/plugins/zotero.lua
@@ -1,6 +1,5 @@
 vim.opt.rtp:prepend(vim.fn.expand("~/Documents/GitHub/zotero.nvim") --[[@as string]])
 
 require("zotero").setup({
-  backend = "fzf",
-  max_items = "3000",
+  max_items = 3000,
 })
